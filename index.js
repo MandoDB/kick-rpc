bb09bd67ce010f9591519d8e86e6a8db78c084f6
TheHellTower <dontbeshybuddy@gmail.com>
2023-06-14 10:12:19 -0400

    Small Update...

--- index.js
+++ index.js
@@ -1,37 +1,25 @@
-const clientId = '';
-const DiscordRPC = require('discord-rpc');
-const RPC = new DiscordRPC.Client({ transport: 'ipc'});
-
-DiscordRPC.register(clientId);
+const { clientId, RPC: { details, largeImageKey, smallImageKey, buttons }} = require("./config.json");
+const RPC = new (require('discord-rpc')).Client({ transport: 'ipc'});
 
 async function setActivity() {
     if(!RPC) return;
-    RPC.setActivity({ // Put here what you want, your links, your stream titles, the image etc...
-        details: `Rejoins l'aventure sur Starbound !`,
+    RPC.setActivity({
+        details: details,
         startTimestamp: Date.now(),
-        largeImageKey: 'kicklogo',
-        smallImageKey: 'kicklogo',
+        largeImageKey: largeImageKey,
+        smallImageKey: smallImageKey,
         instance: false,
-        buttons: [ //Here you put the buttons you want (label = title, url = link).
-            {
-                label: `Rejoins moi sur Kick !`,
-                url: 'https://kick.com/mandodb'
-            },
-            {
-                label: `Rejoins le Discord !`,
-                url: 'https://discord.gg/la-station'
-            }
-        ]
+        buttons: buttons
     });
 };
 
-RPC.on('ready', async () => { // RPC.on run the script
+RPC.on('ready', async () => {
     console.log(`Script is running now...`); 
     setActivity();
 
-    setInterval(() => { //With setInterval the script will check that the status is still working.
+    setInterval(() => {
         setActivity();
     }, 15 * 1000); 
 });
 
-RPC.login({ clientId }).catch(err => console.error(err)); //Le script se connecte au systeme de status et vérifie que l'ID fonctionne.
+RPC.login({ clientId }).catch(err => console.error(err));
